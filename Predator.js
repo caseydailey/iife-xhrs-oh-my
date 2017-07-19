6fbf2d88d853291551c8c60b403752bd1939f66b
Casey Dailey <casey@feasttogether.org>
2017-07-19 14:09:05 -0500

    refactor and comments

--- Predator.js
+++ Predator.js
@@ -1,41 +1,61 @@
 
 var Predator = (function (predator) {
-  var carnivores = [];
-  var herbivores = [];
 
-  
+    var carnivores = [];
+    var herbivores = [];
 
     predator.loadCarnivores = function (callBack) {
+
+      //make a new request
       var request = new XMLHttpRequest();
+
+      //open and send it
       request.open("GET", "carnivores.json");
       request.send();
-    request.addEventListener("error", loadFailed);
-      request.addEventListener("load", function (load) {
-        // Set the value of the private array
+
+      //listen for load and/or error
+      request.addEventListener("error", loadFailed);
+      request.addEventListener("load", loadComplete);
+
+      //callback for load
+      function loadComplete(load) {
         carnivores = JSON.parse(this.responseText);
         callBack(carnivores);
-      });
+      }
+
+      //callback for error
+      function loadFailed(load){
+        console.log("loaded failed: ", load.target.responseText);
+      };
     }
 
-    function loadFailed(load){
-      console.log("loaded failed: ", load.target.responseText);
-    };
 
-      predator.loadHerbivores = function (callBack) {
+
+    predator.loadHerbivores = function (callBack) {
+
+      //create a new request
       var request = new XMLHttpRequest();
+
+      //open and send it
       request.open("GET", "herbivores.json");
       request.send();
-    request.addEventListener("error", loadFailed);
-      request.addEventListener("load", function (load) {
-        // Set the value of the private array
+
+      //listen for load and/or errors
+      request.addEventListener("error", loadFailed);
+      request.addEventListener("load", loadComplete);
+
+      //callback for load
+      function loadComplete(load) {
         herbivores = JSON.parse(this.responseText);
         callBack(herbivores);
-      });
-    }
+      }
 
-    function loadFailed(load){
-      console.log("loaded failed: ", load.target.responseText);
-    };
+      //callback for error
+      function loadFailed(load){
+        console.log("loaded failed: ", load.target.responseText);
+      };
+
+    }
 
     return predator;
 
--- iife-xhr.js
+++ iife-xhr.js
@@ -2,11 +2,11 @@ const carnList = document.querySelector("#carnivores");
 const herbList = document.querySelector("#herbivores");
 
 function showCarnivores (carnivores) {
-  carnivores.predators.forEach((carnivore)=> carnList.innerHTML += `<li>${carnivore.name}</li>`);
+  carnivores.animals.forEach((carnivore)=> carnList.innerHTML += `<li>${carnivore.name}</li>`);
 }
 
 function showHerbivores (herbivores) {
-  herbivores.herbivores.forEach((herbivore)=> herbList.innerHTML += `<li>${herbivore.name}</li>`);
+  herbivores.animals.forEach((herbivore)=> herbList.innerHTML += `<li>${herbivore.name}</li>`);
 
 }
 
